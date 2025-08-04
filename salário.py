494d4608e2ec3b6902c7b303616026f29d4d6d9b
Elliot8064 <ncoelhodalmeida@gmail.com>
2025-08-04 17:00:27 +0100

    Add files via upload

---
+++ salário.py
@@ -0,0 +1,7 @@
+print("salário por horas")
+salario = int(input("insira o salário"))
+horas = int(input("agora insira as horas"))
+
+resultado = (salario/horas)
+
+print (f'O funcionário recebe {resultado} por hora')
